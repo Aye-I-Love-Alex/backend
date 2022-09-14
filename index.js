6caac0daf0d70cd21561d23f675ef0184a49f203
Joel Yates <59893600+JoelYatesCollege@users.noreply.github.com>
2022-09-13 23:29:09 -0400

    separated javascript into separate file

---
+++ index.js
@@ -0,0 +1,8 @@
+var first, second;
+
+function findConnection(){
+    first = document.getElementById("firstword").value;
+    second = document.getElementById("secondword").value;
+    console.log(first);
+    console.log(second);
+}
