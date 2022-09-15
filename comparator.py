23597a25bdc811a800fe40389fa75657a739a3c2
Joel Yates <59893600+JoelYatesCollege@users.noreply.github.com>
2022-09-15 17:24:57 -0400

    set up flask server in python file

--- comparator.py
+++ comparator.py
@@ -2,24 +2,29 @@ import wikipediaapi
 from flask import Flask
 
 wikipedia = wikipediaapi.Wikipedia('en')
+app = Flask(__name__)
 
-first_topic = input("First topic: ")
-first_page = wikipedia.page(first_topic)
 
-second_topic = input("Second topic: ")
-second_page = wikipedia.page(second_topic)
+@app.route('/connection', methods=['GET'])
+def connection():
+    first_topic = input("First topic: ")
+    first_page = wikipedia.page(first_topic)
 
-first_links = set(first_page.links.keys())
-second_links = set(second_page.links.keys())
-common_links = first_links.intersection(second_links)
+    second_topic = input("Second topic: ")
+    second_page = wikipedia.page(second_topic)
 
-if second_topic in first_links:
-    common_links.add(second_topic)
+    first_links = set(first_page.links.keys())
+    second_links = set(second_page.links.keys())
+    common_links = first_links.intersection(second_links)
 
-if first_topic in second_links:
-    common_links.add(first_topic)
+    if second_topic in first_links:
+        common_links.add(second_topic)
 
-print(common_links)
+    if first_topic in second_links:
+        common_links.add(first_topic)
+
+    #print(common_links)
+    return "hello world"
 
 
 
