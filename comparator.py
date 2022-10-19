ef73faa3b5544292d9711c67832d5609f4ba7bf6
awimmel <alex.wimmel@gmail.com>
2022-10-18 23:55:31 -0400

    Working graphing for bidirectional BFS

--- comparator.py
+++ comparator.py
@@ -35,19 +35,53 @@ def index():
         if len(first) > 0 and len(second) > 0:
             connection = Connection(first, second)
             path = connection.find_all_connections()
-            print(path)
-            # path = connection.find_connection()
-            # if len(path) > 0:
-            #     source = []
-            #     dest = []
-            #     for path_index in range(len(path) - 1):
-            #         source.append(path[path_index])
-            #         dest.append(path[path_index + 1])
-            #     df = pd.DataFrame({"Source": source, "Target": dest, "Weight": [1] * len(source)})
-            #     graph = nx.from_pandas_edgelist(df, source="Source", target="Target", edge_attr="Weight")
-            #     net = Network()
-            #     net.from_nx(graph)
-            #     net.save_graph("./graph/graph.html")
+            if len(path) > 0:
+                graph = nx.Graph()
+                nodes = []
+                # Iterating through each path that was found
+                for path_index in range(len(path)):
+                    current_path = path[path_index]
+                    parent = ""
+
+                    # Iterating through each element in the current path
+                    for element_index in range(len(current_path)):
+                        # Ensuring that the current title has not already been added as a node
+                        if current_path[element_index] not in nodes:
+                            # Checking current index because that changes how the node's colors work
+                            if (
+                                element_index == 0
+                                or element_index == len(current_path) - 1
+                            ):
+                                graph.add_node(current_path[element_index], color="red")
+                            else:
+                                graph.add_node(current_path[element_index])
+
+                            # Ensuring that we don't add the node twice
+                            nodes.append(current_path[element_index])
+
+                        # Checking if an edge should be added or not. Assigning parent accordingly
+                        if element_index == 0:
+                            parent = current_path[element_index]
+                        else:
+                            # Checking current index because that changes how the edge's colors work
+                            # Adding edges to graph
+                            if element_index == len(current_path) - 1:
+                                graph.add_edge(
+                                    parent,
+                                    current_path[element_index],
+                                    weight=1,
+                                    color="red",
+                                )
+                            else:
+                                graph.add_edge(
+                                    parent, current_path[element_index], weight=1
+                                )
+                            parent = current_path[element_index]
+
+                net = Network()
+                net.from_nx(graph)
+                net.save_graph("./graph/graph.html")
+                
     return render_template("index.html", connection=net)
 
 
