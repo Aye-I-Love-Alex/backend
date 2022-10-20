46e8a983d936a5c431e9a76bf0fdf7bdf5206c96
Joel Yates <59893600+JoelYatesCollege@users.noreply.github.com>
2022-10-20 11:38:52 -0400

    decomposed method for easier testing, made max_itr a param with default arg for coding practices

--- Connection.py
+++ Connection.py
@@ -1,6 +1,7 @@
 from elasticsearch import Elasticsearch
 from ConnectionInterface import ConnectionInterface
 
+DEFAULT_MAX_ITR = 1000
 
 class Connection(ConnectionInterface):
 
@@ -20,6 +21,7 @@ class Connection(ConnectionInterface):
         self.first_page, self.first_links, self.first_topic = self.get_page_info(first_topic)
         self.second_page, self.second_links, self.second_topic = self.get_page_info(second_topic)
 
+    # get page, links, and title in a tuple from a topic
     def get_page_info(self, topic):
         page = self.es.search(
             index="wikipedia_pages", body={"query": {"match": {"title": topic}}}
@@ -29,13 +31,15 @@ class Connection(ConnectionInterface):
         title = []
         # if there are no hits in the database, leave arrays blank
         if len(hits) > 0:
+            links = hits[0]["_source"]["links"]
+            title = hits[0]["_source"]["title"]
             # return the info for the hit with the most outgoing links
-            max_links = 0
-            for hit in hits:
-                if max_links < len(hit["_source"]["links"]):
-                    max_links = len(hit["_source"]["links"])
-                    links = hit["_source"]["links"]
-                    title = hit["_source"]["title"]
+            # max_links = 0
+            # for hit in hits:
+            #     if max_links < len(hit["_source"]["links"]):
+            #         max_links = len(hit["_source"]["links"])
+            #         links = hit["_source"]["links"]
+            #         title = hit["_source"]["title"]
         return page, links, title
 
     # Generating path when BFS finds a path
@@ -59,8 +63,15 @@ class Connection(ConnectionInterface):
 
         return path
 
+    # return dict with all links referring to topic
+    def init_parents(self, links, topic):
+        parents = {}
+        for link in links:
+            parents[link] = topic
+        return parents
+
     # Bidirectional BFS logic here
-    def find_all_connections(self):
+    def find_all_connections(self, max_iter=DEFAULT_MAX_ITR):
 
         paths = []
         # Storing all of the links for the first and second topics separately
@@ -68,22 +79,14 @@ class Connection(ConnectionInterface):
         second_topic_links = self.second_links.copy()
 
         # Storing the parents for each link
-        first_parents = {}
-        for link in self.first_links:
-            first_parents[link] = self.first_topic
-
-        second_parents = {}
-        for link in self.second_links:
-            second_parents[link] = self.second_topic
+        first_parents = self.init_parents(self.first_links, self.first_topic)
+        second_parents = self.init_parents(self.second_links, self.second_topic)
 
         # Storing the seen links for the first and second topic separately
-        first_seen_links = []
-        first_seen_links.append(self.first_topic)
-        second_seen_links = []
-        second_seen_links.append(self.second_topic)
+        first_seen_links = [self.first_topic]
+        second_seen_links = [self.second_topic]
 
-        # Only iterating for 1000 iterations currently
-        max_iter = 1000
+        # Initialize current iteration counter
         current_iter = 0
 
         # Only continuing while there are still links in both topics and the maximum number of iterations
@@ -91,8 +94,6 @@ class Connection(ConnectionInterface):
         while (
             len(first_topic_links) != 0 or len(second_topic_links) != 0
         ) and current_iter < max_iter:
-            current_link = ""
-
             # Only continuing if there are links remaining for the first topic
             if len(first_topic_links) != 0:
                 current_link = first_topic_links.pop(0)
@@ -152,38 +153,3 @@ class Connection(ConnectionInterface):
 
         # Returning array of array of paths
         return paths
-
-    # BFS logic here for MVP
-    def find_connection(self):
-        first_links = self.first_page["hits"]["hits"][0]["_source"]["links"]
-        first_topic = self.first_page["hits"]["hits"][0]["_source"]["title"]
-        parent_queue = [first_topic] * len(first_links)
-
-        second_topic = self.second_page["hits"]["hits"][0]["_source"]["title"]
-
-        path = []
-        seen_links = first_links.copy()
-        seen_links.append(first_topic)
-        cont = True
-        while len(first_links) != 0 and cont:
-            current_link = first_links.pop(0)
-            self.result_tree[current_link] = parent_queue.pop(0)
-            if current_link == second_topic:
-                iter_back = current_link
-                while iter_back != first_topic:
-                    path.append(iter_back)
-                    iter_back = self.result_tree[iter_back]
-                path.append(first_topic)
-                cont = False
-            else:
-                result_links = self.es.search(
-                    index="wikipedia_pages",
-                    body={"query": {"match": {"title": current_link}}},
-                )["hits"]["hits"][0]["_source"]["links"]
-                for link in result_links:
-                    if link not in seen_links:
-                        seen_links.append(link)
-                        first_links.append(link)
-                        parent_queue.append(current_link)
-        path.reverse()
-        return path
