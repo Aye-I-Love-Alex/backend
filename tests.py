af58bc5bda2c12c40347dd5085b329cb28a42ac0
Joel Yates <59893600+JoelYatesCollege@users.noreply.github.com>
2022-10-20 18:12:41 -0400

    removed unnecessary print lines

--- tests.py
+++ tests.py
@@ -8,7 +8,6 @@ def test_init_parents_1_normal():
     topic = 'topic'
     parents = Connection.init_parents(None, links, topic)
     expected_parents = {'link1': 'topic', 'link2': 'topic', 'link3': 'topic', 'link4': 'topic', 'link5': 'topic'}
-    print(parents)
     assert parents == expected_parents, 'Incorrect return for dictionary of links to topics.'
     assert links == ['link1', 'link2', 'link3', 'link4', 'link5'], 'First parameter was unintentionally altered.'
     assert topic == 'topic', 'Second parameter was unintentionally altered.'
@@ -19,7 +18,6 @@ def test_init_parents_2_no_links():
     topic = 'topic'
     parents = Connection.init_parents(None, links, topic)
     expected_parents = {}
-    print(parents)
     assert parents == expected_parents, 'Incorrect return for dictionary of links to topics.'
     assert links == [], 'First parameter was unintentionally altered.'
     assert topic == 'topic', 'Second parameter was unintentionally altered.'
