01c57b7ce626827085e9e4a7ac037ab2024e1da1
Joel Yates <59893600+JoelYatesCollege@users.noreply.github.com>
2022-10-27 21:43:55 -0400

    revised find all connections, im pushing cuz i wanna save where im at ty

--- Connection.py
+++ Connection.py
@@ -1,6 +1,9 @@
 from elasticsearch import Elasticsearch
 from ConnectionInterface import ConnectionInterface
+import time
+import operator
 
+MAX_ITER = 24
 
 class Connection(ConnectionInterface):
 
@@ -17,24 +20,19 @@ class Connection(ConnectionInterface):
     result_tree = {}
 
     def __init__(self, first_topic, second_topic):
-        self.first_page = self.es.search(
-            index="wikipedia_pages", body={"query": {"match": {"title": first_topic}}}
-        )
+        self.first_page = self.es.search(index="wikipedia_pages", body={"query": {"match": {"title": first_topic}}})
         if len(self.first_page["hits"]["hits"]) > 0:
             self.first_links = self.first_page["hits"]["hits"][0]["_source"]["links"]
             self.first_topic = self.first_page["hits"]["hits"][0]["_source"]["title"]
 
-        self.second_page = self.es.search(
-            index="wikipedia_pages", body={"query": {"match": {"title": second_topic}}}
-        )
+        self.second_page = self.es.search(index="wikipedia_pages", body={"query": {"match": {"title": second_topic}}})
         if len(self.second_page["hits"]["hits"]) > 0:
             self.second_links = self.second_page["hits"]["hits"][0]["_source"]["links"]
             self.second_topic = self.second_page["hits"]["hits"][0]["_source"]["title"]
 
     # Generating path when BFS finds a path
     def generate_path(self, first_parents, second_parents, common):
-        path = []
-        path.append(common)
+        path = [common]
         child = common
 
         # Adding to front for first topic's path
@@ -53,8 +51,8 @@ class Connection(ConnectionInterface):
         return path
 
     # Bidirectional BFS logic here
-    def find_all_connections(self):
-
+    def find_all_connections(self, max_iter=MAX_ITER):
+        start_time = time.time()
         paths = []
         # Storing all of the links for the first and second topics separately
         first_topic_links = self.first_links.copy()
@@ -62,22 +60,22 @@ class Connection(ConnectionInterface):
 
         # Storing the parents for each link
         first_parents = {}
+        first_seen_links = {self.first_topic}
         for link in self.first_links:
             first_parents[link] = self.first_topic
+            first_seen_links.add(link)
 
         second_parents = {}
+        second_seen_links = {self.second_topic}
         for link in self.second_links:
             second_parents[link] = self.second_topic
-
-        # Storing the seen links for the first and second topic separately
-        first_seen_links = []
-        first_seen_links.append(self.first_topic)
-        second_seen_links = []
-        second_seen_links.append(self.second_topic)
+            second_seen_links.add(link)
 
         # Only iterating for 1000 iterations currently
-        max_iter = 1000
         current_iter = 0
+        query_time = 0.0
+        queried_links = {}
+        num_queries = 0
 
         # Only continuing while there are still links in both topics and the maximum number of iterations
         # has not been reached yet
@@ -86,97 +84,75 @@ class Connection(ConnectionInterface):
         ) and current_iter < max_iter:
             current_link = ""
 
-            # Only continuing if there are links remaining for the first topic
+            # pop off queue
             if len(first_topic_links) != 0:
                 current_link = first_topic_links.pop(0)
 
-                # Path found in this case, appending to list of completed paths
+                # check if the node has been seen
                 if current_link in second_seen_links:
-                    paths.append(
-                        self.generate_path(first_parents, second_parents, current_link)
+                    # if so, generate path, clear other nodes expanded from it
+                    paths.append(self.generate_path(first_parents, second_parents, current_link))
+                else:
+                    # if not, expand
+                    if current_link in queried_links:
+                        queried_links[current_link] += 1
+                    else:
+                        queried_links[current_link] = 1
+                    num_queries += 1
+                    start = time.time()
+                    result = self.es.search(
+                        index="wikipedia_pages",
+                        body={"query": {"match": {"title": current_link}}},
                     )
+                    query_time += time.time() - start
+                    if len(result["hits"]["hits"]) > 0:
+                        result_links = result["hits"]["hits"][0]["_source"]["links"]
+                        for link in result_links:
+                            if link not in first_seen_links:
+                                first_topic_links.append(link)
+                                first_seen_links.add(link)
+                                first_parents[link] = current_link
 
-                # Appending to seen links
-                if current_link not in first_seen_links:
-                    first_seen_links.append(current_link)
-
-                # Expanding current_link and storing for future work
-                result = self.es.search(
-                    index="wikipedia_pages",
-                    body={"query": {"match": {"title": current_link}}},
-                )
-
-                # Ensuring there are actual results, storing links from the current page and
-                # storing the children for future expansion and saving the parents of the children
-                if len(result["hits"]["hits"]) > 0:
-                    result_links = result["hits"]["hits"][0]["_source"]["links"]
-                    for link in result_links:
-                        if link not in first_seen_links:
-                            first_topic_links.append(link)
-                            first_parents[link] = current_link
-
-            # Only continuing if there are links remaining for the second topic
+            # pop off queue
             if len(second_topic_links) != 0:
                 current_link = second_topic_links.pop(0)
+
+                # check if the node has been seen
                 if current_link in first_seen_links:
-                    # Path found in this case
-                    paths.append(
-                        self.generate_path(first_parents, second_parents, current_link)
+                    # if so, generate path, clear other nodes expanded from it
+                    paths.append(self.generate_path(first_parents, second_parents, current_link))
+                else:
+                    # if not, expand
+                    if current_link in queried_links:
+                        queried_links[current_link] += 1
+                    else:
+                        queried_links[current_link] = 1
+                    num_queries += 1
+                    start = time.time()
+                    result = self.es.search(
+                        index="wikipedia_pages",
+                        body={"query": {"match": {"title": current_link}}},
                     )
-
-                # Appending to seen links if necessary
-                if current_link not in second_seen_links:
-                    second_seen_links.append(current_link)
-
-                # Ensuring there are actual results, storing links from the current page and
-                # storing the children for future expansion and saving the parents of the children
-                result = self.es.search(
-                    index="wikipedia_pages",
-                    body={"query": {"match": {"title": current_link}}},
-                )
-                if len(result["hits"]["hits"]) > 0:
-                    result_links = result["hits"]["hits"][0]["_source"]["links"]
-                    for link in result_links:
-                        if link not in second_seen_links:
-                            second_topic_links.append(link)
-                            second_parents[link] = current_link
+                    query_time += time.time() - start
+                    if len(result["hits"]["hits"]) > 0:
+                        result_links = result["hits"]["hits"][0]["_source"]["links"]
+                        for link in result_links:
+                            if link not in second_seen_links:
+                                second_topic_links.append(link)
+                                second_seen_links.add(link)
+                                second_parents[link] = current_link
 
             current_iter += 1
 
+        print('Query time: %s seconds' % query_time)
+        print('Runtime: %s seconds' % (time.time() - start_time))
+        sorted_queried_links = dict(sorted(queried_links.items(), key=operator.itemgetter(1), reverse=True))
+        new = {}
+        for k, n in sorted_queried_links.items():
+            if n > 1:
+                new[k] = n
+        print(new)
+        print('Number of total queries: ' + str(num_queries))
+        print('Number of distinct queries: ' + str(len(queried_links)))
         # Returning array of array of paths
         return paths
-
-    # BFS logic here for MVP
-    def find_connection(self):
-        first_links = self.first_page["hits"]["hits"][0]["_source"]["links"]
-        first_topic = self.first_page["hits"]["hits"][0]["_source"]["title"]
-        parent_queue = [first_topic] * len(first_links)
-
-        second_topic = self.second_page["hits"]["hits"][0]["_source"]["title"]
-
-        path = []
-        seen_links = first_links.copy()
-        seen_links.append(first_topic)
-        cont = True
-        while len(first_links) != 0 and cont:
-            current_link = first_links.pop(0)
-            self.result_tree[current_link] = parent_queue.pop(0)
-            if current_link == second_topic:
-                iter_back = current_link
-                while iter_back != first_topic:
-                    path.append(iter_back)
-                    iter_back = self.result_tree[iter_back]
-                path.append(first_topic)
-                cont = False
-            else:
-                result_links = self.es.search(
-                    index="wikipedia_pages",
-                    body={"query": {"match": {"title": current_link}}},
-                )["hits"]["hits"][0]["_source"]["links"]
-                for link in result_links:
-                    if link not in seen_links:
-                        seen_links.append(link)
-                        first_links.append(link)
-                        parent_queue.append(current_link)
-        path.reverse()
-        return path
