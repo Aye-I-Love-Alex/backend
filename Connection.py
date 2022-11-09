85c34fcba406af42c471a6b652870e9c8df3f039
awimmel <alex.wimmel@gmail.com>
2022-11-09 01:52:55 -0500

    Bulk search for BFS

--- Connection.py
+++ Connection.py
@@ -86,13 +86,30 @@ class Connection(ConnectionInterface):
 
         links_to_expand = math.ceil(self.intensity * len(self.first_links))
         links_inserted = 0
+
+        first_to_search = []
         for link in self.first_links:
             if link not in first_parents:
-                first_topic_links.append(link)
+                # first_topic_links.append(link)
+                first_to_search.append({"index": "wikipedia_pages"})
+                first_to_search.append(
+                    {"query": {"match": {"_id": link}}}
+                )
                 first_parents[link] = self.first_topic
                 links_inserted += 1
             if links_inserted == links_to_expand:
                 break
+        
+        if len(first_to_search) != 0:
+            results = self.es.msearch(body=first_to_search)
+            for new_link in results["responses"]:
+                if len(new_link['hits']['hits']) > 0:
+                    title = new_link['hits']['hits'][0]['_id']
+                    links = new_link['hits']['hits'][0]['_source']['links']
+                    first_topic_links.append({
+                        'title': title,
+                        'links': links
+                    })
 
         second_topic_links = []
         second_parents = {self.second_topic: None}
@@ -100,20 +117,38 @@ class Connection(ConnectionInterface):
 
         links_to_expand = math.ceil(self.intensity * len(self.second_links))
         links_inserted = 0
+        
+        second_to_search = []
         for link in self.second_links:
             if link not in second_parents:
-                second_topic_links.append(link)
+                # second_topic_links.append(link)
+                second_to_search.append({"index": "wikipedia_pages"})
+                second_to_search.append(
+                    {"query": {"match": {"_id": link}}}
+                )
                 second_parents[link] = self.second_topic
                 links_inserted += 1
 
             if links_inserted == links_to_expand:
                 break
+        
+        if len(second_to_search) != 0:
+            results = self.es.msearch(body=second_to_search)
+            for new_link in results["responses"]:
+                if len(new_link['hits']['hits']) > 0:
+                    title = new_link['hits']['hits'][0]['_id']
+                    links = new_link['hits']['hits'][0]['_source']['links']
+                    second_topic_links.append({
+                        'title': title,
+                        'links': links
+                    })
 
         # Initialize iteration counter
         current_iter = 0
 
         valid_max_iter = 1000 if not max_iter.isnumeric() else int(max_iter)
-
+        first_to_search = []
+        second_to_search = []
         # Only continuing while there are still links in both topics and the maximum number of iterations
         # has not been reached yet
         while (
@@ -125,60 +160,80 @@ class Connection(ConnectionInterface):
                 current_link = first_topic_links.pop(0)
 
                 # check if the node has been seen by the other BFS direction (second link)
-                if current_link in second_parents:
+                if current_link['title'] in second_parents:
                     # if so, generate a path and add to the list of paths for graph generation
                     paths.append(
-                        self.generate_path(first_parents, second_parents, current_link)
+                        self.generate_path(first_parents, second_parents, current_link['title'])
                     )
                 else:
                     # if not, expand and add new links if we have not already seen them from this direction
-                    result = self.es.search(
-                        index="wikipedia_pages",
-                        body={"query": {"match": {"title": current_link}}},
-                    )
-                    first_seen_links.add(current_link)
-                    if len(result["hits"]["hits"]) > 0:
-                        result_links = result["hits"]["hits"][0]["_source"]["links"]
-                        links_to_expand = math.ceil(self.intensity * len(result_links))
-                        links_inserted = 0
-                        for link in result_links:
-                            if link not in first_parents:
-                                first_topic_links.append(link)
-                                first_parents[link] = current_link
-                                links_inserted += 1
+                    first_seen_links.add(current_link['title'])
+                    result_links = current_link['links']
+                    links_to_expand = math.ceil(self.intensity * len(result_links))
+                    links_inserted = 0
+                    for link in result_links:
+                        if link not in first_parents:
+                            # first_topic_links.append(link)
+                            first_to_search.append({"index": "wikipedia_pages"})
+                            first_to_search.append(
+                                {"query": {"match": {"_id": link}}}
+                            )
+                            first_parents[link] = current_link['title']
+                            links_inserted += 1
 
-                            if links_inserted == links_to_expand:
-                                break
+                        if links_inserted == links_to_expand:
+                            break
+
+            if (len(first_to_search) != 0 and len(first_topic_links) != 0) or len(first_to_search) == 100:
+                # Bulk search logic here
+                results = self.es.msearch(body=first_to_search)
+                for new_link in results["responses"]:
+                    if 'hits' in new_link and 'hits' in new_link['hits'] and len(new_link['hits']['hits']) > 0:
+                        title = new_link['hits']['hits'][0]['_id']
+                        links = new_link['hits']['hits'][0]['_source']['links']
+                        first_topic_links.append({
+                            'title': title,
+                            'links': links
+                        })
+                first_to_search = []
 
             # pop off queue of links to explore
             if len(second_topic_links) != 0:
                 current_link = second_topic_links.pop(0)
 
                 # check if the node has been seen by the other BFS direction (first link)
-                if current_link in first_parents:
+                if current_link['title'] in first_parents:
                     # if so, generate a path and add to the list of paths for graph generation
                     paths.append(
-                        self.generate_path(first_parents, second_parents, current_link)
+                        self.generate_path(first_parents, second_parents, current_link['title'])
                     )
                 else:
-                    # if not, expand and add new links if we have not already seen them from this direction
-                    result = self.es.search(
-                        index="wikipedia_pages",
-                        body={"query": {"match": {"title": current_link}}},
-                    )
-                    second_seen_links.add(current_link)
-                    if len(result["hits"]["hits"]) > 0:
-                        result_links = result["hits"]["hits"][0]["_source"]["links"]
-                        links_to_expand = math.ceil(self.intensity * len(result_links))
-                        links_inserted = 0
-                        for link in result_links:
-                            if link not in second_parents:
-                                second_topic_links.append(link)
-                                second_parents[link] = current_link
-                                links_inserted += 1
+                    second_seen_links.add(current_link['title'])
+                    result_links = current_link['links']
+                    links_to_expand = math.ceil(self.intensity * len(result_links))
+                    links_inserted = 0
+                    for link in result_links:
+                        if link not in second_parents:
+                            second_to_search.append({"index": "wikipedia_pages"})
+                            second_to_search.append(
+                                {"query": {"match": {"_id": link}}}
+                            )
+                            second_parents[link] = current_link['title']
+                            links_inserted += 1
 
-                            if links_inserted == links_to_expand:
-                                break
+                        if links_inserted == links_to_expand:
+                            break
+            if (len(second_to_search) != 0 and len(second_to_search) != 0) or len(second_to_search) == 100:
+                results = self.es.msearch(body=second_to_search)
+                for new_link in results["responses"]:
+                    if 'hits' in new_link and 'hits' in new_link['hits'] and len(new_link['hits']['hits']) > 0:
+                        title = new_link['hits']['hits'][0]['_id']
+                        links = new_link['hits']['hits'][0]['_source']['links']
+                        second_topic_links.append({
+                            'title': title,
+                            'links': links
+                        })
+                second_to_search = []
 
             current_iter += 1
 
